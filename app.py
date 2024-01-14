458bdb984ee58b9b156f72501be49fe8d265c9e1
Alex <alexpuello788@gmail.com>
2024-01-14 16:56:31 -0400

    Update the methods create_book and get_books for work with the new connection method

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import data.db as db
 import jwt
+import json
+import sqlalchemy
 from cryptography.hazmat.primitives import serialization
 
 db.init_database()
 app = Flask(__name__)
 
-#CREATE BOOK
+
+# CREATE BOOK
 @app.post('/books')
 def create_book():
-    data = request.get_json() 
+    data = request.get_json()
     title = data["title"]
     category = data["category"]
 
     with db.connection_pool.connect() as db_conn:
         db_conn.execute(db.CREATE_BOOK_TABLE)
         db_conn.execute(db.INSERT_INTO_BOOK_TABLE, parameters={"title": title, "category": category})
-    return "book created successfully"
+        db_conn.commit()
+    return "book created successfully", 200
 
 
-#GET ALL THE BOOKS CONTENT
+# GET ALL THE BOOKS CONTENT
 @app.get("/books")
 def get_books():
-    with db.connection:
-        with db.connection.cursor() as cursor:
-            cursor.execute(db.GET_ALL_THE_BOOKS)
-            result = cursor.fetchall()
-    
-            books_dict = []
-            for book in result:
-                books_dict.append({"id":book[0], "title": book[1], "category":book[2]})
-            return books_dict
+    with db.connection_pool.connect() as db_conn:
+        result = db_conn.execute(db.GET_ALL_THE_BOOKS).fetchall()
+        books_dict = []
+        for row in result:
+            books_dict.append({"id":row[0], "title": row[1], "category":row[2]})
+        return json.dumps(books_dict)
 
-        
-#GET A BOOK BY ITS ID
+
+
+# GET A BOOK BY ITS ID
 @app.get("/books/<int:id>")
 def get_book(id):
     with db.connection:
@@ -46,8 +48,9 @@ def get_book(id):
                 return (result_json)
             else:
                 return f"Error: Book with id {id} not found", 404
-    
-#UPDATE A BOOK
+
+
+# UPDATE A BOOK
 @app.put("/books/<int:id_book>")
 def book_update(id_book):
     data = request.get_json()
@@ -63,11 +66,12 @@ def book_update(id_book):
             else:
                 return (f"Error 404: Book with id {id_book} not found"), 404
 
-#Delete book
+
+# Delete book
 @app.delete('/books/<int:id_book>')
 def book_delete(id_book):
     with db.connection:
-        with db.connection.cursor() as cursor: 
+        with db.connection.cursor() as cursor:
             cursor.execute(db.DELETE_BOOK, (id_book,))
 
             if cursor.rowcount > 0:
@@ -75,9 +79,10 @@ def book_delete(id_book):
             else:
                 return (f"Book with id {id_book} not found"), 404
 
-#========================================================================================================
 
-#CREATE A CATEGORY
+# ========================================================================================================
+
+# CREATE A CATEGORY
 @app.post('/category')
 def create_category():
     data = request.get_json()
@@ -87,9 +92,9 @@ def create_category():
             cursor.execute(db.CREATE_CATEGORY_TABLE)
             cursor.execute(db.INSERT_INTO_CATEGORY, (category_name,))
         return "Category created successfully"
-    
 
-#SELECT ALL FROM CATEGORY
+
+# SELECT ALL FROM CATEGORY
 @app.get('/category')
 def categories():
     all_categories = []
@@ -98,24 +103,26 @@ def categories():
             cursor.execute(db.SELECT_ALL_FROM_CATEGORY)
             result = cursor.fetchall()
             for category in result:
-                all_categories.append({"category_id": category[0], "category_name":category[1]})
+                all_categories.append({"category_id": category[0], "category_name": category[1]})
             return all_categories
-            
-#SELECT A CATEGORY BY ITS ID
+
+
+# SELECT A CATEGORY BY ITS ID
 @app.get('/category/<int:id_category>')
 def get_category(id_category):
     with db.connection:
         with db.connection.cursor() as cursor:
             cursor.execute(db.SELECT_CATEGORY_BY_ID, (id_category,))
-            
+
             if cursor.rowcount > 0:
                 result_list = cursor.fetchone()
                 result_json = {"category_id": result_list[0], "category_name": result_list[1]}
                 return result_json, 200
             else:
-                return(f"Category with id {id_category} does not exist"), 404
-            
-#UPDATE A CATEGORY
+                return (f"Category with id {id_category} does not exist"), 404
+
+
+# UPDATE A CATEGORY
 @app.put('/category/<int:id_category>')
 def update_category(id_category):
     data = request.get_json()
@@ -126,9 +133,10 @@ def update_category(id_category):
             if cursor.rowcount > 0:
                 return f"Category with id {id_category} was updated successfully", 200
             else:
-                return f"Category with id {id_category} does not exist",404
-        
-#DELETE A CATEGORY
+                return f"Category with id {id_category} does not exist", 404
+
+
+# DELETE A CATEGORY
 @app.delete('/category/<int:id_category>')
 def remove_category(id_category):
     with db.connection:
@@ -136,44 +144,47 @@ def remove_category(id_category):
             cursor.execute(db.DELETE_CATEGORY, (id_category,))
             if cursor.rowcount > 0:
                 return f"Category with id {id_category} deleted", 200
-            
+
             else:
                 return f"Category with id {id_category} does not exist", 404
 
 
-#========================================================================================================
-#TOKEN GENERATOR
+# ========================================================================================================
+# TOKEN GENERATOR
 def jwt_converter(datos):
     header = {
-    "alg": "HS256",
-    "typ": "JWT"
+        "alg": "HS256",
+        "typ": "JWT"
     }
     payload_data = datos
     secret_key = "ThisIsthesecretkey"
 
-    token = jwt.encode( payload_data, secret_key, algorithm='HS256', headers=header)    
+    token = jwt.encode(payload_data, secret_key, algorithm='HS256', headers=header)
     return token
 
-#CREATE_USER
-@app.post("/user")     
-def create_user():
 
+# CREATE_USER
+@app.post("/user")
+def create_user():
     data = request.get_json()
     data_for_token = {"name": data["name"], "lastname": data["lastname"], "username": data["username"]}
-    
+
     token = jwt_converter(data_for_token)
-    
+
     with db.connection:
         with db.connection.cursor() as cursor:
             cursor.execute(db.CREATE_USER)
-            cursor.execute(db.INSER_INTO_USER, (data["name"], data["lastname"], data["username"], data["email"], data["password"], data["created_at"], token))
+            cursor.execute(db.INSER_INTO_USER, (
+            data["name"], data["lastname"], data["username"], data["email"], data["password"], data["created_at"],
+            token))
 
             if cursor.rowcount > 0:
                 return f"Account created Successfully And the jwt is \n{token}\n", 200
             else:
                 return f"something went wrong"
 
-#GET ALL THE USERS
+
+# GET ALL THE USERS
 @app.get("/user")
 def request_users():
     with db.connection:
@@ -183,10 +194,13 @@ def request_users():
             users = []
 
             for user in accounts:
-                users.append({"name": user[0], "lastname": user[1], "username": user[2], "email": user[3], "password": user[4], "created_at": user[5]})
+                users.append(
+                    {"name": user[0], "lastname": user[1], "username": user[2], "email": user[3], "password": user[4],
+                     "created_at": user[5]})
             return users
 
-#GET A USER
+
+# GET A USER
 @app.get("/user/<string:token>")
 def request_a_user(token):
     with db.connection:
@@ -194,13 +208,15 @@ def request_a_user(token):
             cursor.execute(db.GET_A_USER, (token,))
             if cursor.rowcount > 0:
                 data = cursor.fetchone()
-                data_json = {"name": data[0], "lastname" : data[1], "username" : data[2], "email": data[3], "password": data[4], "created_at": data[5], "update_at": data[6]}
-                
+                data_json = {"name": data[0], "lastname": data[1], "username": data[2], "email": data[3],
+                             "password": data[4], "created_at": data[5], "update_at": data[6]}
+
                 return data_json, 200
             else:
                 return "Unauthorized access", 401
 
-#UPDATE A USER
+
+# UPDATE A USER
 @app.put("/user/<string:token>")
 def update_password(token):
     data = request.get_json()
@@ -217,7 +233,8 @@ def update_password(token):
             else:
                 return "Unauthorized access", 401
 
-#DELETE A USER
+
+# DELETE A USER
 @app.delete("/user/<string:token>")
 def delete_user(token):
     with db.connection:
@@ -227,4 +244,3 @@ def delete_user(token):
                 return "User account delete successfully", 200
             else:
                 return "Unauthorized access", 401
-
